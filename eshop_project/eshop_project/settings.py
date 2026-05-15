6d476462625ea88317b3d6fe821286d35f3e181f
Vijay Gayagol <vijaygayagol123@gmail.com>
2026-05-15 11:35:42 +0530

    database updated

--- eshop_project/eshop_project/settings.py
+++ eshop_project/eshop_project/settings.py
@@ -81,13 +81,17 @@ WSGI_APPLICATION = 'eshop_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
